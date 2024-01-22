712775bb483e5ef8f42ea1ad71dbc70d9a4550b2
Moein Madadi <moeinmadadi2002@gmail.com>
2024-01-22 18:46:55 +0330

    deletes fix_random

--- utils/aggregate_block/fix_random.py
+++ utils/aggregate_block/fix_random.py
@@ -17,9 +17,9 @@ def fix_random(
     :param random_seed:
     :return: None
     '''
-    random.seed(random_seed)
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # random.seed(random_seed)
+    # np.random.seed(random_seed)
+    # torch.manual_seed(random_seed)
+    # torch.cuda.manual_seed_all(random_seed)
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
