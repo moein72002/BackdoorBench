e4382121a3aad656796c0e38a98429a8eb07af88
Moein Madadi <moeinmadadi2002@gmail.com>
2024-01-23 15:43:44 +0330

    deletes bothering prints

--- utils/trainer_cls.py
+++ utils/trainer_cls.py
@@ -1869,14 +1869,14 @@ class BackdoorModelTrainer(ModelTrainerCLS_v2):
         with torch.no_grad():
             for batch_idx, (x, labels, original_index, poison_indicator, original_targets) in enumerate(test_dataloader):
                 x = x.to(device, non_blocking=self.non_blocking)
-                print(f"original_targets[:5]: {original_targets[:5]}")
+                # print(f"original_targets[:5]: {original_targets[:5]}")
                 original_targets = original_targets.to(device, non_blocking=self.non_blocking)
                 pred = model(x)
 
                 #TODO: check below
                 normality_scores = torch.max(pred.detach().cpu(), dim=1).values
-                print(f"pred.size(): {pred.size()}")
-                print(f"normality_scores.size(): {normality_scores.size()}")
+                # print(f"pred.size(): {pred.size()}")
+                # print(f"normality_scores.size(): {normality_scores.size()}")
 
                 if verbose == 1:
                     batch_label_list.append(original_targets.detach().clone().cpu())
