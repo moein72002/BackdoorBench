ae798d6deb6c67ecedb876626e77fca5074ffca2
Moein Madadi <moeinmadadi2002@gmail.com>
2024-02-06 16:26:55 +0330

    visualize bd_train (train) dataset

--- attack/badnet.py
+++ attack/badnet.py
@@ -227,7 +227,7 @@ class BadNet(NormalCase):
         bd_test_dataset_with_transform_ood, \
             = self.stage1_results
 
-        self.visualize_random_samples_from_bd_dataset(bd_train_dataset_with_transform, "bd_train_dataset_with_transform")
+        self.visualize_random_samples_from_bd_dataset(bd_train_dataset_with_transform.wrapped_dataset, "bd_train_dataset_with_transform.wrapped_dataset")
 
         self.net = generate_cls_model(
             model_name=args.model,
