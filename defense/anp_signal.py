c9250c64894967dc8adba26d91c47415904898bf
Moein Madadi <moeinmadadi2002@gmail.com>
2024-04-26 23:57:37 +0330

    fix

--- defense/anp_signal.py
+++ defense/anp_signal.py
@@ -586,7 +586,6 @@ if __name__ == '__main__':
     elif args.attack == "blended":
         set_blended_bd_args(parser)
     print(f"args.bd_yaml_path: {args.bd_yaml_path}")
-    print(f"args.attack_label_trans: {args.attack_label_trans}")
     add_bd_yaml_to_args(args)
     print(f"args.attack_label_trans: {args.attack_label_trans}")
     add_yaml_to_args(args)
