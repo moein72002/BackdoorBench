b0157d00b46b8759787590b898057fe48346706f
Moein Madadi <moeinmadadi2002@gmail.com>
2024-04-29 10:55:57 +0330

    ignore .idea and removes fix_random

--- utils/aggregate_block/fix_random.py
+++ utils/aggregate_block/fix_random.py
@@ -17,9 +17,10 @@ def fix_random(
     :param random_seed:
     :return: None
     '''
-    random.seed(random_seed)
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    pass
+    # random.seed(random_seed)
+    # np.random.seed(random_seed)
+    # torch.manual_seed(random_seed)
+    # torch.cuda.manual_seed_all(random_seed)
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
